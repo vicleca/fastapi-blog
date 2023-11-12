c2894c52ca509b10c1cb3ad752147ee28a70e667
vicleca <victorleocanto@gmail.com>
2023-11-12 19:24:02 +0100

    Add image upload endpoint

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 from database import models
 from database.database import engine
 from routers import post
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.include_router(post.router)
 
 models.Base.metadata.create_all(engine)
+
+app.mount('/images', StaticFiles(directory='images'), name='images')
--- routers/post.py
+++ routers/post.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, File, UploadFile
 from routers.schemas import PostBase, PostDisplay
 from sqlalchemy.orm import Session
 from database.database import get_db
 from database import db_post
+import string
+import random
+import shutil
 
 router = APIRouter(
     prefix='/post',
@@ -20,3 +23,16 @@ def posts(db: Session = Depends(get_db)):
 @router.delete('/{id}')
 def delete(id: int, db: Session = Depends(get_db)):
     return db_post.delete(id, db)
+
+@router.post('/image')
+def upload_image(image: UploadFile = File(...)):
+    letters = string.ascii_letters
+    rand_str = ''.join(random.choice(letters) for i in range(6))
+    new = f'_{rand_str}.'
+    filename = new.join(image.filename.rsplit('.', 1))
+    path = f'images/{filename}'
+
+    with open(path, "w+b") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+
+    return {'filename': path}
